9cd07247dc89c3b79cc8a9490ceeb8f7c1cf90dc
unknown <emrah.pekesen@emrahpekesen-nb.ISTDOM.com>
2016-03-08 15:20:05 +0200

    Java 8 Implementation..

--- src/main/java/com/aep/training/ci/app/App.java
+++ src/main/java/com/aep/training/ci/app/App.java
@@ -7,15 +7,23 @@ import com.aep.training.ci.model.Member;
 public class App {
 
 	public static void main(String[] args) {
-	     Initializer.init();
-	     checkMissingFlyers();
-	     FakeDB.displayAllMissingFlyers();
+		Initializer.init();
+		checkMissingFlyers();
+		FakeDB.displayAllMissingFlyers();
+		try {
+			FakeDB.insertReservation(new Member().memberId("1241932125")
+										         .firstName("Emrah")
+										         .lastName("PEKESEN")
+										         .pnr("XD45GT"));
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		};
+
 	}
-	
-	public static void checkMissingFlyers(){
-		for(Member m: FakeDB.getReservationList())
-		{
-			if(!FakeDB.getCheckInList().contains(m))
+
+	public static void checkMissingFlyers() {
+		for (Member m : FakeDB.getReservationList()) {
+			if (!FakeDB.getCheckInList().contains(m))
 				FakeDB.getMissingFlyersList().add(m);
 		}
 	}
--- src/main/java/com/aep/training/ci/model/Member.java
+++ src/main/java/com/aep/training/ci/model/Member.java
@@ -4,13 +4,17 @@ import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
+import lombok.experimental.Accessors;
 
 @Data
 @EqualsAndHashCode(exclude = { "uniqueId" })
 @ToString(exclude = { "uniqueId" })
 @AllArgsConstructor(access = AccessLevel.PUBLIC)
+@NoArgsConstructor(access = AccessLevel.PUBLIC)
+@Accessors(fluent=true)
 public class Member {
 	@Setter(AccessLevel.PRIVATE)
 	private int uniqueId;
