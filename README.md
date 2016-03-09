258dade40a0e53a7e40815b00360a64295758434
unknown <emrah.pekesen@emrahpekesen-nb.ISTDOM.com>
2016-03-09 14:45:55 +0200

    some tests are added

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Just a step by step CI with java/travis/codecov -----
 
 [![Build Status](https://travis-ci.org/emrahpekesen/java_travis_CI.svg?branch=master)](https://travis-ci.org/emrahpekesen/java_travis_CI)      [![Code Coverage](https://img.shields.io/codecov/c/github/emrahpekesen/java_travis_CI/master.svg)](https://codecov.io/github/emrahpekesen/java_travis_CI?branch=master)
+
+[comment]: <> (This is a comment, it will not be included)
--- src/test/java/com/aep/training/ci/model/test/Base_Test.java
+++ src/test/java/com/aep/training/ci/model/test/Base_Test.java
@@ -3,12 +3,19 @@ package com.aep.training.ci.model.test;
 import static org.junit.Assert.*;
 
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
+import com.aep.training.ci.fakedb.FakeDB;
 import com.aep.training.ci.model.Member;
 import com.aep.training.ci.model.MemberValidator;
 
+
 public class Base_Test {
+
+	@Rule
+	public final ExpectedException exception = ExpectedException.none();
 
 	private Member member;
 
@@ -35,5 +42,30 @@ public class Base_Test {
 		assertFalse(MemberValidator.checkMemberId(member));
 		member.memberId(null);
 		assertFalse(MemberValidator.checkMemberId(member));
+		member.memberId("1245655998");
+		assertTrue(MemberValidator.checkMemberId(member));
+	}
+
+	@Test // duplicate reservation check
+	public void insertNewReservation() throws Exception  {
+		exception.expect(Exception.class);
+		FakeDB.insertReservation(member);
+		FakeDB.insertReservation(member);
+		
+	}
+	
+	@Test // duplicate checkin check
+	public void insertNewCheckIn() throws Exception  {
+		exception.expect(Exception.class);
+		FakeDB.insertCheckIn(member);
+		FakeDB.insertCheckIn(member);
+		
+	}
+	
+	@Test // duplicate missing flyer check
+	public void insertNewMissingFlyer() throws Exception  {
+		exception.expect(Exception.class);
+		FakeDB.insertMissingFlyer(member);
+		FakeDB.insertMissingFlyer(member);
 	}
 }
